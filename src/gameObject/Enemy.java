19ebbd92b2678c854b7925ef61d02cd6d7f97fc8
jonathon-b <jonathonbower14@gmail.com>
2020-01-27 11:36:47 -0800

    made new GameObject, Enemy class

--- src/gameObject/Enemy.java
+++ src/gameObject/Enemy.java
@@ -2,14 +2,10 @@ package gameObject;
 
 import java.awt.geom.Rectangle2D;
 
-public class Enemy extends Rectangle2D.Double{
-	double dx,dy;
+public class Enemy extends GameObject{
 
-	public Enemy(double x, double y, double dx, double dy, double size){
-		super(0,0,size, size);
-		this.dx=dx; this.dy=dy;
+	public Enemy(double x, double y, double dx, double dy, double size) {
+		super(x, y, dx, dy, size);
 	}
-
-
 
 }
---
+++ src/gameObject/GameObject.java
@@ -0,0 +1,22 @@
+package gameObject;
+
+import java.awt.*;
+import java.awt.geom.Rectangle2D;
+
+public class GameObject extends Rectangle2D.Double {
+	double dx,dy;
+
+	public GameObject(double x, double y, double dx, double dy, double size){
+		super(x,y,size, size);
+		this.dx=dx; this.dy=dy;
+	}
+
+	public void update(){
+		this.x+=dx; this.y+=dy;
+	}
+
+	public void drawBound(Graphics2D win, Color c){
+		win.setColor(c);
+		win.draw(this);
+	}
+}
